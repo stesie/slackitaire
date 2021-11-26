f81130433c027004bc129435a8112ff528fe65ec
Stefan Siegl <stesie@brokenpipe.de>
2021-11-26 11:46:13 +0100

    remove GameEngine class, export turn function

--- cli.ts
+++ cli.ts
@@ -1,4 +1,22 @@
 import { EnglishBoard } from "./src/board/EnglishBoard";
+import { turn } from "./src/GameEngine";
 import { AsciiRenderer } from "./src/render/AsciiRenderer";
 
-console.log(new AsciiRenderer().render(new EnglishBoard()));
+console.log(
+  new AsciiRenderer().render(
+    turn(
+      turn(new EnglishBoard(), {
+        startX: 1,
+        startY: 3,
+        endX: 3,
+        endY: 3,
+      }),
+      {
+        startX: 2,
+        startY: 1,
+        endX: 2,
+        endY: 3,
+      }
+    )
+  )
+);
--- src/GameEngine.test.ts
+++ src/GameEngine.test.ts
@@ -1,10 +1,10 @@
 import { EnglishBoard } from "./board/EnglishBoard";
-import { GameEngine, Turn } from "./GameEngine";
+import { turn, Turn } from "./GameEngine";
 
 describe("#turn", () => {
   it("checks start for valid position", () => {
     expect(() =>
-      new GameEngine().turn(new EnglishBoard(), {
+      turn(new EnglishBoard(), {
         startX: 1,
         startY: 1,
         endX: 3,
@@ -15,7 +15,7 @@ describe("#turn", () => {
 
   it("checks end for valid position", () => {
     expect(() =>
-      new GameEngine().turn(new EnglishBoard(), {
+      turn(new EnglishBoard(), {
         startX: 3,
         startY: 3,
         endX: 1,
@@ -26,7 +26,7 @@ describe("#turn", () => {
 
   it("fails if there is no peg at start position", () => {
     expect(() =>
-      new GameEngine().turn(new EnglishBoard(), {
+      turn(new EnglishBoard(), {
         startX: 3,
         startY: 3,
         endX: 3,
@@ -37,7 +37,7 @@ describe("#turn", () => {
 
   it("fails if there is a peg at end position", () => {
     expect(() =>
-      new GameEngine().turn(new EnglishBoard(), {
+      turn(new EnglishBoard(), {
         startX: 0,
         startY: 3,
         endX: 2,
@@ -56,8 +56,8 @@ describe("#turn", () => {
     { startX: 3, startY: 2, endX: 3, endY: 3 },
     { startX: 3, startY: 4, endX: 3, endY: 3 },
     { startX: 3, startY: 6, endX: 3, endY: 3 },
-  ])("fails on too long or too short turns (%s)", (turn: Turn) => {
-    expect(() => new GameEngine().turn(new EnglishBoard(), turn)).toThrow();
+  ])("fails on too long or too short turns (%s)", (t: Turn) => {
+    expect(() => turn(new EnglishBoard(), t)).toThrow();
   });
 
   it.each([
@@ -65,8 +65,8 @@ describe("#turn", () => {
     { startX: 5, startY: 3, endX: 3, endY: 3 },
     { startX: 3, startY: 1, endX: 3, endY: 3 },
     { startX: 3, startY: 5, endX: 3, endY: 3 },
-  ])("does not throw on valid turns", (turn: Turn) => {
-    expect(() => new GameEngine().turn(new EnglishBoard(), turn)).not.toThrow();
+  ])("does not throw on valid turns", (t: Turn) => {
+    expect(() => turn(new EnglishBoard(), t)).not.toThrow();
   });
 
   it.each([
@@ -74,17 +74,17 @@ describe("#turn", () => {
     [{ startX: 5, startY: 3, endX: 3, endY: 3 }, [4, 3]],
     [{ startX: 3, startY: 1, endX: 3, endY: 3 }, [3, 2]],
     [{ startX: 3, startY: 5, endX: 3, endY: 3 }, [3, 4]],
-  ])("returns a updated board on a valid turn", (turn, [middleX, middleY]) => {
-    const result = new GameEngine().turn(new EnglishBoard(), turn);
+  ])("returns a updated board on a valid turn", (t, [middleX, middleY]) => {
+    const result = turn(new EnglishBoard(), t);
 
-    expect(result.hasPegAt(turn.startX, turn.startY)).toBe(false);
+    expect(result.hasPegAt(t.startX, t.startY)).toBe(false);
     expect(result.hasPegAt(middleX, middleY)).toBe(false);
-    expect(result.hasPegAt(turn.endX, turn.endY)).toBe(true);
+    expect(result.hasPegAt(t.endX, t.endY)).toBe(true);
   });
 
   it("fails if middle peg is missing", () => {
     expect(() =>
-      new GameEngine().turn(new EnglishBoard().withoutPegAt(2, 3), {
+      turn(new EnglishBoard().withoutPegAt(2, 3), {
         startX: 1,
         startY: 3,
         endX: 3,
@@ -95,7 +95,7 @@ describe("#turn", () => {
 
   it("fails on diagonal turns", () => {
     expect(() =>
-      new GameEngine().turn(new EnglishBoard().withoutPegAt(2, 4), {
+      turn(new EnglishBoard().withoutPegAt(2, 4), {
         startX: 0,
         startY: 2,
         endX: 2,
