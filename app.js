df84f7f839320ce67d84a1d85f97e1d3fb6ddd96
Stefan Siegl <stesie@brokenpipe.de>
2021-11-26 09:04:37 +0100

    try app home example

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 // Require the Bolt package (github.com/slackapi/bolt)
 const { App } = require("@slack/bolt");
 
@@ -9,6 +11,78 @@ const app = new App({
 
 
 // All the room in the world for your code
+app.event('app_home_opened', async ({ event, client, context }) => {
+	console.log('got app_home_opened', event, context); 
+
+	/* got app_home_opened {
+  type: 'app_home_opened',
+  user: 'U3NUYU4KU',
+  channel: 'D02P1UCE51P',
+  tab: 'messages',
+  event_ts: '1637913541.054887'
+} {
+  isEnterpriseInstall: false,
+  botToken: 'xxxx',
+  botUserId: 'U02NGJNHQCW',
+  botId: 'B02P1UBKM33',
+  teamId: 'T04NUEFRH',
+  retryNum: undefined,
+  retryReason: undefined,
+  updateConversation: [Function (anonymous)]
+}
+ */
+
+  try {
+    /* view.publish is the method that your app uses to push a view to the Home tab */
+    const result = await client.views.publish({
+
+      /* the user that opened your app's app home */
+      user_id: event.user,
+
+      /* the view object that appears in the app home*/
+      view: {
+        type: 'home',
+        callback_id: 'home_view',
+
+        /* body of the view */
+        blocks: [
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "*Welcome to your _App's Home_* :tada:"
+            }
+          },
+          {
+            "type": "divider"
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
+            }
+          },
+          {
+            "type": "actions",
+            "elements": [
+              {
+                "type": "button",
+                "text": {
+                  "type": "plain_text",
+                  "text": "Click me!"
+                }
+              }
+            ]
+          }
+        ]
+      }
+    });
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
 
 
 
